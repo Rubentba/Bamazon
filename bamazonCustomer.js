03830f41a2a27745962ce1d5a3201c14637fd51d
Rubentba <rubenatorresbeltran@gmail.com>
2019-06-06 02:28:51 -0400

    Got database table to display with node.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,4 +11,39 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     console.log("Connected as id: " + connection.threadId);
+    // start();
 })
+
+connection.query("SELECT * FROM products", function(err, res){
+      
+  if(err) throw err
+
+  console.log("Check out our selection...\n")
+  console.log("  ID  |      Product Name      |  Department Name  |   Price  | In Stock")
+  console.log("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ")
+  
+  for(let i = 0; i < res.length; i++){
+
+    let itemId = res[i].item_id,
+        productName = res[i].product_name,
+        departmentName = res[i].department_name,
+        price = "$" + res[i].price,
+        quantity = res[i].stock_quantity
+
+    console.log(itemId + "|" + productName + "|" + departmentName + "|" + price + "|" + quantity)
+  }
+})
+
+// var start = function() {
+//     inquirer.prompt({
+//         name:"selectRole",
+//         type:"rawlist",
+//         message: "Choose you role.",
+//         choices: ["Customer"]
+//     }).then(function(answer){
+//         if(answer.selectRole.toUpperCase() == "Customer") {
+//             // goCustomer();
+//         }
+    
+//     })
+// }
